0667a08992758558efbdc53db90b5ebede21eb48
Vadym <vadym3110mh@gmail.com>
2025-12-03 19:35:00 +0200

    Add project files.

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,25 @@
+﻿namespace ChessLogic
+{
+    public enum Player
+    {
+        White,
+        Black,
+        None
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Opponent(this Player player)
+        {
+            switch(player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default:
+                    return Player.None;
+            }
+        }
+    }
+}
---
+++ ChessUI/GameOverMenu.xaml.cs
@@ -0,0 +1,65 @@
+﻿using ChessLogic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ChessUI
+{
+    /// <summary>
+    /// Interaction logic for GameOverMenu.xaml
+    /// </summary>
+    public partial class GameOverMenu : UserControl
+    {
+        public event Action<Option> OptionSelected;
+
+        public GameOverMenu(GameState gameState)
+        {
+            InitializeComponent();
+
+            Result result = gameState.Result;
+            WinnerText.Text = GetWinnerText(result.Winner);
+            ReasonText.Text = GetReasonText(result.Reason, gameState.CurrentPlayer);
+        }
+        private static string GetWinnerText(Player winner)
+        {
+            return winner switch
+            {
+                Player.White => "WHITE WINS!",
+                Player.Black => "BLACK WINS!",
+                _ => "IT'S A DRAW"
+            };
+        }
+
+        private static string PLayerString(Player player)
+        {
+            return player switch
+            {
+                Player.White => "White",
+                Player.Black => "Black",
+                _ => ""
+            };
+        }
+
+        private static string GetReasonText(EndReason reason, Player currentPlayer)
+        {
+            return reason switch
+            {
+                EndReason.Stalemate => $"Stalemate - {PLayerString(currentPlayer)} can't move",
+                EndReason.Checkmate => $"Checkmate - {PLayerString(currentPlayer)} can't move",
+                EndReason.FiftyMoveRule => "Fifty-move rule",
+                EndReason.InsufficientMaterial => "Insufficient material",
+                EndReason.ThreefoldRepetition => "Threefold repetition",
+                _ => ""
+            };
+        }
+
+        private void Restart_Click(object sender, RoutedEventArgs e)
+        {
+            OptionSelected?.Invoke(Option.Restart);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            OptionSelected?.Invoke(Option.Exit);
+        }
+    }
+}
